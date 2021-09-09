b4e53c1836da01f50a5637740ae8c42d0ac58427
Ashfaque P P <60512006+ash-faque@users.noreply.github.com>
2021-09-09 18:57:15 +0530

    00000

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 // env
-require('dotenv').config();
+//require('dotenv').config();
 // twitter
 const Twitter = require('twitter');
 const client = new Twitter({
